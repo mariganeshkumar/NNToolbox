2a42f61ee8e4ca7b76cff02bb1581faf92b77a32
Mari Ganesh Kumar M <mariganeshkumar@gmail.com>
2017-01-31 22:53:55 +0530

    MnistHandler Updated

--- src/MnistHandler/MnistHandler.py
+++ src/MnistHandler/MnistHandler.py
@@ -1,8 +1,9 @@
 import cPickle, gzip
 
+
 # Load the dataset
 def readMNISTData(path):
     f = gzip.open(path, 'rb')
     train_set, valid_set, test_set = cPickle.load(f)
     f.close()
-    return train_set,valid_set,test_set
+    return train_set, valid_set, test_set
