5ee31aa3784b38154ff449b9c34b8af32f557cf5
Mari Ganesh Kumar M <mariganeshkumar@gmail.com>
2017-02-01 11:48:02 +0530

    tangent sigmoid added

--- src/activations/sigmoid.py
+++ src/activations/sigmoid.py
@@ -5,13 +5,26 @@ def LogSigmoidWithBias(data,weights,biases,validationRequired=True):
     # data is Dim(L-1)*NoOfExaples, Weights is Dim(L)*Dim(L-1), Biases is Dim(L)*1
     if validationRequired:
         data,weights=IntergrateBiasWithWeightsAndData(data,weights,biases)
-    return logSigmoid(data,weights,False)
+    return LogSigmoid(data, weights, False)
 
-def logSigmoid(data,weights,validationRequired=True):
+def LogSigmoid(data, weights, validationRequired=True):
     # data is Dim(L-1)+1(bias)*NoOfExaples, Weights is Dim(L)+1*Dim(L-1)
     if validationRequired:
         ValidateDimensions(data,weights)
     return np.divide(1.0,(1.0+ np.exp(np.matmul(weights,data))))
+
+def TanSigmoidWithBias(data,weights,biases,validationRequired=True):
+    # data is Dim(L-1)*NoOfExaples, Weights is Dim(L)*Dim(L-1), Biases is Dim(L)*1
+    if validationRequired:
+        data,weights=IntergrateBiasWithWeightsAndData(data,weights,biases)
+    return TanSigmoid(data, weights, False)
+
+def TanSigmoid(data,weights,validationRequired=True):
+    # data is Dim(L-1)+1(bias)*NoOfExaples, Weights is Dim(L)+1*Dim(L-1)
+    if validationRequired:
+        ValidateDimensions(data,weights)
+    preActivation=np.matmul(weights,data)
+    return np.divide(np.exp(preActivation)-np.exp(-preActivation),np.exp(preActivation)+np.exp(-preActivation))
 
 #######################################################################################################################
 def ValidateDimensionsWithBias(data,weights,biases):
