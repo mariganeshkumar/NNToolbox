611e3a3af95dfb66ec2870f00f93cdbb1157172f
Mari Ganesh Kumar M <mariganeshkumar@gmail.com>
2017-02-01 11:31:25 +0530

    activation redefined

--- src/activations/sigmoid.py
+++ src/activations/sigmoid.py
@@ -1,21 +1,36 @@
 import numpy as np
 
 
-
-def logSigmoidWithBias(data,weights,biases):
+def LogSigmoidWithBias(data,weights,biases,validationRequired=True):
     # data is Dim(L-1)*NoOfExaples, Weights is Dim(L)*Dim(L-1), Biases is Dim(L)*1
-    if(data.shape[0]!=weights.shape[1] or biases.shape[0]!=weights.shape[0]):
-        print('Error, please check the domension of input matrices')
-        print('data:', data.shape, 'weights', weights.shape,'biases:',biases.shape)
-        return
-    data = np.append(data,np.ones((1,data.shape[1])),axis=0)
-    weights=np.append(weights,biases,axis=1)
-    return logSigmoid(data,weights)
+    if validationRequired:
+        data,weights=IntergrateBiasWithWeightsAndData(data,weights,biases)
+    return logSigmoid(data,weights,False)
 
-def logSigmoid(data,weights):
+def logSigmoid(data,weights,validationRequired=True):
     # data is Dim(L-1)+1(bias)*NoOfExaples, Weights is Dim(L)+1*Dim(L-1)
-    if (data.shape[0] != weights.shape[1] ):
-        print('Error, please check the domension of input matrices')
-        print('data:',data.shape,'weights',weights.shape)
-        return
+    if validationRequired:
+        ValidateDimensions(data,weights)
     return np.divide(1.0,(1.0+ np.exp(np.matmul(weights,data))))
+
+#######################################################################################################################
+def ValidateDimensionsWithBias(data,weights,biases):
+    # Validates data to be Dim(L-1)*NoOfExaples, Weights to be Dim(L)*Dim(L-1), Biases to be Dim(L)*1
+    if (data.shape[0] != weights.shape[1] or biases.shape[0] != weights.shape[0]):
+        print('Error, please check the domension of input matrices')
+        print('data:', data.shape, 'weights', weights.shape, 'biases:', biases.shape)
+        raise ValueError('Incorrect dimmension given to sigmoid function')
+
+def ValidateDimensions(data, weights):
+    # Validates data to be Dim(L-1)*NoOfExaples, Weights to be Dim(L)*Dim(L-1), Biases to be Dim(L)*1
+    if (data.shape[0] != weights.shape[1]):
+        print('Error, please check the domension of input matrices')
+        print('data:', data.shape, 'weights', weights.shape)
+        raise ValueError('Incorrect dimmension given to sigmoid function')
+
+def IntergrateBiasWithWeightsAndData(data, weights, biases):
+    # Input: data is Dim(L-1)*NoOfExaples, Weights is Dim(L)*Dim(L-1), Biases is Dim(L)*1
+    # Output: data is Dim(L-1)+1(bias)*NoOfExaples, Weights is Dim(L)+1*Dim(L-1)
+    data = np.append(data, np.ones((1, data.shape[1])), axis=0)
+    weights = np.append(weights, biases, axis=1)
+    return data, weights
