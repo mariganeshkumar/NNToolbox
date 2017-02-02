fixed redudant computation in sigmoid gradients

src/Activations/Sigmoid.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from src.Utility import CommonUtilityFunctions as cuf
+
 from src.Utility.CommonUtilityFunctions import ValidateDimensionsWithBiasAndOutput, ValidateDimensionsWithOutput, \
     ValidateDimensionsWithBias, ValidateDimensions, IntergrateBiasWithWeightsAndData
 
@@ -34,11 +36,11 @@ def TanSigmoid(data,weights,validationRequired=True):
 #######################################Sigmoid Gradiants###############################################################
 
 
-def LogSigmoidGradientsWithBias(data, weights, biases, gradients, validationRequired=True,biasRequired=True):
+def LogSigmoidGradientsWithBias(activations, weights, biases, gradients, validationRequired=True,biasRequired=True):
     if validationRequired:
-        ValidateDimensionsWithBiasAndOutput(data, weights, biases, gradients)
-    data,weightsWithBias= IntergrateBiasWithWeightsAndData(data, weights, biases)
-    gradients= LogSigmoidGradients(data, weightsWithBias, gradients, False)
+        cuf.ValidateDimensionsWithBiasActivationAndGradients(activations, weights, biases, gradients)
+    weightsWithBias= cuf.IntergrateBiasWithWeights(weights, biases)
+    gradients= LogSigmoidGradients(activations, weightsWithBias, gradients, False)
     if biasRequired:
         weightsGradients=gradients[:,0:weights.shape[1]]
         biasesGradients = gradients[:, weights.shape[1]:weights.shape[1]+1]
@@ -47,17 +49,16 @@ def LogSigmoidGradientsWithBias(data, weights, biases, gradients, validationRequ
         return gradients
 
 
-def LogSigmoidGradients(data, weights, gradients, validationRequired=True):
+def LogSigmoidGradients(activations, weights, gradients, validationRequired=True):
     if validationRequired:
-        ValidateDimensionsWithOutput(data, weights, gradients)
-    activations=LogSigmoid(data,weights,False)
+        cuf.ValidateDimensionsWithActivationAndGradients(activations, weights, gradients)
     return np.matmul(np.multiply(np.multiply( gradients, activations), (1 - activations)), np.transpose(data))
 
-def TanSigmoidGradientsWithBias(data, weights, biases, gradients, validationRequired=True, biasRequired=True):
+def TanSigmoidGradientsWithBias(activations, weights, biases, gradients, validationRequired=True, biasRequired=True):
     if validationRequired:
-        ValidateDimensionsWithBiasAndOutput(data, weights, biases, gradients)
-    data,weightsWithBias= IntergrateBiasWithWeightsAndData(data, weights, biases)
-    gradients= TanSigmoidGradients(data, weightsWithBias, gradients, False)
+        cuf.ValidateDimensionsWithBiasActivationAndGradients(activations, weights, biases, gradients)
+    weightsWithBias= cuf.IntergrateBiasWithWeights( weights, biases)
+    gradients= TanSigmoidGradients(activations, weightsWithBias, gradients, False)
     if biasRequired:
         weightsGradients=gradients[:,0:weights.shape[1]]
         biasesGradients = gradients[:, weights.shape[1]:weights.shape[1]+1]
@@ -66,10 +67,9 @@ def TanSigmoidGradientsWithBias(data, weights, biases, gradients, validationRequ
         return gradients
 
 
-def TanSigmoidGradients(data, weights, gradients, validationRequired=True):
+def TanSigmoidGradients(activations, weights, gradients, validationRequired=True):
     if validationRequired:
-        ValidateDimensionsWithOutput(data, weights, gradients)
-    activations=TanSigmoid(data,weights,False)
+        cuf.ValidateDimensionsWithBiasActivationAndGradients(activations, weights, gradients)
     return np.matmul(np.multiply(np.multiply(gradients, (1 + activations)), (1 - activations)), np.transpose(data))
 
 

src/Utility/CommonUtilityFunctions.py
@@ -24,6 +24,14 @@ def ValidateDimensionsWithBias(data,weights,biases):
         print('data:', data.shape, 'weights', weights.shape, 'biases:', biases.shape)
         raise ValueError('Incorrect dimmension given to sigmoid function')
 
+def   ValidateDimensionsWithBiasActivationAndGradients(activation,weights,biases):
+    #todo: Complete Function
+    pass
+
+def   ValidateDimensionsWithActivationAndGradients(activation,weights,biases):
+    #todo: Complete Function
+    pass
+
 
 def ValidateDimensions(data, weights):
     # Validates data to be Dim(L-1)*NoOfExaples, Weights to be Dim(L)*Dim(L-1), Biases to be Dim(L)*1
@@ -36,6 +44,19 @@ def ValidateDimensions(data, weights):
 def IntergrateBiasWithWeightsAndData(data, weights, biases):
     # Input: data is Dim(L-1)*NoOfExaples, Weights is Dim(L)*Dim(L-1), Biases is Dim(L)*1
     # Output: data is Dim(L-1)+1(bias)*NoOfExaples, Weights is Dim(L)+1*Dim(L-1)
-    data = np.append(data, np.ones((1, data.shape[1])), axis=0)
-    weights = np.append(weights, biases, axis=1)
+    data = IntergrateBiasAndData(data)
+    weights = IntergrateBiasWithWeights(weights,biases)
     return data, weights
+
+def IntergrateBiasAndData(data):
+    # Input: data is Dim(L-1)*NoOfExaples
+    # Output: data is Dim(L-1)+1(bias)*NoOfExaples
+    data = np.append(data, np.ones((1, data.shape[1])), axis=0)
+    return data
+
+def IntergrateBiasWithWeights(weights,biases):
+    # Input: Weights is Dim(L)*Dim(L-1), Biases is Dim(L)*1
+    # Output: Weights is Dim(L)+1*Dim(L-1)
+
+    weights = np.append(weights, biases, axis=1)
+    return weights