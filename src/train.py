067b9e65131d4be7e816646de46048ab93221513
Mari Ganesh Kumar M <mariganeshkumar@gmail.com>
2017-02-02 11:48:55 +0530

    Network Package added

--- src/train.py
+++ src/train.py
@@ -1,23 +1,23 @@
 import numpy as np
 
 from MnistHandler import MnistHandler as mh
-from activations import sigmoid
+from Activations import Sigmoid
 
 trainData,validData,testData=mh.readMNISTData('../../Data/mnist.pkl.gz')
 data=np.array([[0.5,2.5]])
 weights=np.array([[2]])
 biases=np.array([[2]])
 output=np.array([[0.2 , 0.9]])
-layer1=sigmoid.LogSigmoidWithBias(data,weights,biases)
-weightsGradients,biasesGradients=sigmoid.LogSigmoidGradientsWithBias(data,weights,biases,output)
+layer1=Sigmoid.TanSigmoidWithBias(data, weights, biases)
+weightsGradients,biasesGradients=Sigmoid.LogSigmoidGradientsWithBias(data, weights, biases, output)
 print(layer1)
 print('wg',weightsGradients,'bg',biasesGradients)
 print('present l2 loss',np.linalg.norm(output-layer1))
-for i in range(0,500000):
+for i in range(0,50000):
     weights=weights-0.01*weightsGradients
     biases=biases-0.01*biasesGradients
-    layer1=sigmoid.LogSigmoidWithBias(data,weights,biases)
-    weightsGradients,biasesGradients=sigmoid.LogSigmoidGradientsWithBias(data,weights,biases,output)
+    layer1=Sigmoid.TanSigmoidWithBias(data, weights, biases)
+    weightsGradients,biasesGradients=Sigmoid.TanSigmoidGradientsWithBias(data, weights, biases, output)
     #print(output-layer1)
     #print('wg',weightsGradients,'bg',biasesGradients)
     print('present l2 loss',np.linalg.norm(output-layer1))
