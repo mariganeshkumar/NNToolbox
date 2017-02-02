ffa5bc00bc2fdafbbc5e5fe44a1587ba7f184438
Mari Ganesh Kumar M <mariganeshkumar@gmail.com>
2017-02-02 11:24:43 +0530

    train file modified to test

--- src/train.py
+++ src/train.py
@@ -4,13 +4,21 @@ from MnistHandler import MnistHandler as mh
 from activations import sigmoid
 
 trainData,validData,testData=mh.readMNISTData('../../Data/mnist.pkl.gz')
-data=np.array([[0.5,0.2,5,4.5],[-1,0.8,4,5.1]])
-weights=np.array([[0.5,0.2],[1,2],[-0.5,0.2]])
-biases=np.array([[0.5],[0.5],[0.5]])
-output=np.array([[  3.6e-01 ,  3.1e-01,   2.1e-02  , 2.2e-02],
- [  7.3e-01 ,  9.1e-02,   1.3e-06 ,  2.5e-07],
- [  4.8e-01 ,  3.6e-01 ,  7.6e-01 ,  6.7e-01]])
+data=np.array([[0.5,2.5]])
+weights=np.array([[2]])
+biases=np.array([[2]])
+output=np.array([[0.2 , 0.9]])
 layer1=sigmoid.LogSigmoidWithBias(data,weights,biases)
-gradients=sigmoid.LogSigmoidGradientsWithBias(data,weights,biases,output)
-print(output-layer1)
-print(gradients)
+weightsGradients,biasesGradients=sigmoid.LogSigmoidGradientsWithBias(data,weights,biases,output)
+print(layer1)
+print('wg',weightsGradients,'bg',biasesGradients)
+print('present l2 loss',np.linalg.norm(output-layer1))
+for i in range(0,500000):
+    weights=weights-0.01*weightsGradients
+    biases=biases-0.01*biasesGradients
+    layer1=sigmoid.LogSigmoidWithBias(data,weights,biases)
+    weightsGradients,biasesGradients=sigmoid.LogSigmoidGradientsWithBias(data,weights,biases,output)
+    #print(output-layer1)
+    #print('wg',weightsGradients,'bg',biasesGradients)
+    print('present l2 loss',np.linalg.norm(output-layer1))
+print('weights:',weights,'biases',biases)
