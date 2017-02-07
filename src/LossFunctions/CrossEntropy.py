944545ea7a236f5bbf5c20b67ff1139ddf7a1e5b
Mari Ganesh Kumar M <mariganeshkumar@gmail.com>
2017-02-07 21:02:19 +0530

    CrossEntropy Computation optimized

--- src/LossFunctions/CrossEntropy.py
+++ src/LossFunctions/CrossEntropy.py
@@ -5,17 +5,18 @@ def CrossEntropyWithSoftMaxAndBias(data,weights,biases,targetOutput):
     #targetOutput=#noOfClasses*#noOfExamples outputActivafrom OutputFunctions import SoftMaxtion=#noOfClasses*#noOfExamples
     #todo : Add a dimension check and set dimension check to false in softmax call
     outputActivations=SoftMax.SoftMaxWithBias(data,weights,biases)
-    return -np.mean(np.diag(np.matmul(np.transpose(targetOutput),np.log(outputActivations))))
+    return -np.mean(np.einsum('ij,ji->i', np.transpose(targetOutput), np.log(outputActivations)))
 
 def CrossEntropyWithSoftMax(data,weights,targetOutput):
     #targetOutput=#noOfClasses*#noOfExamples outputActivafrom OutputFunctions import SoftMaxtion=#noOfClasses*#noOfExamples
     #todo : Add a dimension check and set dimension check to false in softmax call
     outputActivations=SoftMax.SoftMaxWithBias(data,weights)
-    return -np.mean(np.diag(np.matmul(np.transpose(targetOutput),np.log(outputActivations))))
+    return -np.mean(np.einsum('ij,ji->i', np.transpose(targetOutput), np.log(outputActivations)))
 
 def CrossEntropy(outputActivations,targetOutput):
     #todo : Add a dimension check and set dimension check to false in softmax call and add comment
-    return -np.mean(np.diag(np.matmul(np.transpose(targetOutput),np.log(outputActivations))))
+    return -np.mean(np.einsum('ij,ji->i', np.transpose(targetOutput), np.log(outputActivations)))
+    #-np.mean(np.diag(np.matmul(np.transpose(targetOutput),np.log(outputActivations))))
 
 
 
